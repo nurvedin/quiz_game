84dc583d9a8ab2f730ae69c37a802aadca1495c7
Nurvedin Dizdarevic <nurvedin@hotmail.com>
2019-10-06 21:05:22 +0200

    Added a startgame function that includes a welcome message

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,4 @@
 import { addNickName, startGame } from './quiz-game.js'
-import { getApiReq, answerQuestion } from './fetch-api.js'
 
 addNickName()
 startGame()
-getApiReq()
-answerQuestion(2)
--- src/js/quiz-game.js
+++ src/js/quiz-game.js
@@ -1,4 +1,4 @@
-// import { getApiReq } from './fetch-api'
+import { getQuestion } from './fetch-api.js'
 
 export function addNickName () {
   const nicknameArea = document.querySelector('#quiz-area')
@@ -14,6 +14,7 @@ export function addNickName () {
     if (nicknameField.value.length === 0) {
       element.insertBefore(error, child)
     } else {
+      error.remove()
       button.parentElement.innerHTML = ''
       startGame(nicknameField, nicknameArea)
     }
@@ -28,19 +29,53 @@ export function errorMessage () {
 */
 
 export function startGame (nicknameField, nicknameArea) {
-  const welcomeMessage = document.createElement('h2')
   const child = document.querySelector('#welcome-div')
+  const welcomeMessage = document.createElement('h2')
   const h2 = document.createTextNode(`Welcome ${nicknameField.value}, Lets play!`)
   welcomeMessage.appendChild(h2)
   nicknameArea.insertBefore(welcomeMessage, child)
+  const quizArea = document.querySelector('#quiz-area')
+  quizArea.appendChild(document.querySelector('#welcome').content.cloneNode(true))
   const button = document.querySelector('#play-button')
   // const url = 'http://vhost3.lnu.se:20080/question/1'
   button.addEventListener('click', event => {
-    // getApiReq()
-    console.log('Play')
-  })
+    const question = getQuestion('http://vhost3.lnu.se:20080/question/1')
+    question
+      .then(data => {
+        typeOfQuestion(data)
+      }
+      )
+  }, true)
 }
 
 export function showResults () {
+
+}
+
+function typeOfQuestion (question) {
+  console.log(question)
+  if (typeof question.alternatives !== 'undefined') {
+    console.log('radio')
+  } else {
+    addInputType(question)
+  }
+}
+
+function addInputType () {
+  const answerArea = document.querySelector('#answer-input')
+  const input = document.createElement('INPUT')
+  input.setAttribute('type', 'text')
+  answerArea.appendChild(input)
+
+  const button = document.createElement('BUTTON')
+  button.setAttribute('class', 'btn')
+  button.innerHTML = 'Check answer'
+  answerArea.appendChild(button)
+
+  button.addEventListener('click', event => {
+
+  })
+
+
 
 }
